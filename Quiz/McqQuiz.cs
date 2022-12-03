344b5277ce13a0a6cc7c8607ab4ce378a2b359af
Kanhaya Tyagi <kanhaya.tyagi@successive.tech>
2022-12-03 21:40:29 +0530

    Add project files.

---
+++ Quiz/McqQuiz.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz;
+
+public class McqQuiz
+{
+    public void SetCriteria(int maximumMarks, int minimumMarks)
+    {
+        MaximumMarks = maximumMarks;
+        MiniumMarks = minimumMarks;
+        MarksObtained = 0;
+    }
+    public McqQuiz()
+    {
+        Questions = new List<Question>();
+    }
+    public int MaximumMarks { get; set; } // maximum marks to clear the exam
+    public int MarksObtained { get; set; } // marks obtained by user
+    public int MiniumMarks { get; set; } // minimum marks to clear the paper
+    public string Result
+    {
+        get
+        {
+            if (MarksObtained < MiniumMarks)
+                return "Fail";
+            return "Pass";
+        }
+    }
+    public List<Question> Questions { get; set; }
+}
+
+public class Question
+{
+    public void SetQuestion(int id, string name, Dictionary<string, string> choices, string correctAnswer, int marks)
+    {
+        Id = id;
+        Name = name;
+        Choices = choices;
+        CorrectAnswer = correctAnswer;
+        Marks = marks;
+    }
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public Dictionary<string, string> Choices { get; set; } = null!;
+    public string CorrectAnswer { get; set; } = null!;
+    public int Marks { get; set; }
+    public string SelectedAnswer { get; set; } = null!;
+
+}
---
+++ Quiz/Program.cs
@@ -0,0 +1,104 @@
+﻿using Quiz;
+
+internal class Program
+{
+    public static McqQuiz mcqQuiz;
+
+    public Program()
+    {
+        mcqQuiz = new();
+    }
+
+    public void Init()
+    {
+        mcqQuiz.SetCriteria(100, 33);
+        Console.WriteLine("Welcome to test!");
+        Console.WriteLine($"Maximum number of marks in assement is {mcqQuiz.MaximumMarks} " +
+            $"\nYou must need to obtained {mcqQuiz.MiniumMarks} marks");
+    }
+
+    public void QuestionGenerator()
+    {
+        var choices = new Dictionary<string, string>();
+        // Question 1
+        choices.Add("A", "C#");
+        choices.Add("B", "Java");
+        choices.Add("C", "Python");
+        choices.Add("D", "Javascript");
+        var quesiton1 = new Question();
+        quesiton1.SetQuestion(1, "What is your favourite language?", choices, "A", 25);
+        mcqQuiz.Questions.Add(quesiton1);
+
+        //reset choices 
+        choices = new();
+
+        // Question 2
+        choices.Add("A", "Hibernate");
+        choices.Add("B", "Dapper");
+        choices.Add("C", "Entity Framework");
+        choices.Add("D", "Ado.net");
+        var quesiton2 = new Question();
+        quesiton2.SetQuestion(2, "What is your favourite Framework?", choices, "C", 25);
+        mcqQuiz.Questions.Add(quesiton2);
+
+        //reset choices 
+        choices = new();
+
+        // Question 3
+        choices.Add("A", "Virat Kohli");
+        choices.Add("B", "Rohit sharma");
+        choices.Add("C", "Shikhar Dhawan");
+        choices.Add("D", "Rishabh Pant");
+        var quesiton3 = new Question();
+        quesiton3.SetQuestion(3, "What is your favourite Cricker?", choices, "B", 25);
+        Console.WriteLine("Please enter your answer");
+        mcqQuiz.Questions.Add(quesiton3);
+
+        //reset choices 
+        choices = new();
+
+        // Question 4
+        choices.Add("A", "Nawaz");
+        choices.Add("B", "Hritik Roshan");
+        choices.Add("C", "Varun Dhawan");
+        choices.Add("D", "Kanhaiya Tyagi");
+        var quesiton4 = new Question();
+        quesiton4.SetQuestion(4, "What is your favourite Actor?", choices, "D", 25);
+        mcqQuiz.Questions.Add(quesiton4);
+
+    }
+
+    public void DisplayQuestion()
+    {
+        Console.WriteLine("Question are -");
+        foreach (var question in mcqQuiz.Questions)
+        {
+            Console.WriteLine($"{question.Id} - {question.Name}");
+            foreach (var choice in question.Choices)
+            {
+                Console.WriteLine($"{choice.Key} : {choice.Value}");
+            }
+            Console.WriteLine("Please enter your answer");
+            question.SelectedAnswer = Console.ReadLine();
+        }
+
+    }
+
+    public void GenerateReport()
+    {
+        mcqQuiz.MarksObtained = mcqQuiz.Questions.Where(x => x.CorrectAnswer == x.SelectedAnswer).Sum(x => x.Marks);
+        Console.WriteLine("Assesment completed");
+        Console.WriteLine($"Your result is {mcqQuiz.Result}, you get {mcqQuiz.MarksObtained} marks");
+    }
+
+
+    private static void Main(string[] args)
+    {
+        var program = new Program();
+        program.Init();
+        program.QuestionGenerator();
+        program.DisplayQuestion();
+        program.GenerateReport();
+    }
+
+}
